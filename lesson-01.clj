98c9f32376a6f936adb361ef228427c2a6d22087
Anderson Passos Silva <passossilva.and@gmail.com>
2024-04-04 22:09:43 -0300

    lesson 1: functions and 'parameters'

--- lesson-01.clj
+++ lesson-01.clj
@@ -1,2 +1,14 @@
 (def message "Hello World")
 (println message)
+
+
+; Functions and 'parameters'
+
+(defn function-name
+  [param1 param2]
+  ; body
+  (println "function-body")
+  ; the last line is returned
+  (concat param1 param2)
+
+(println (function-name "Hello" "World!"))
